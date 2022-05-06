9f6d742c545f3d5365b09d1f1d63e8139ff14572
Khaviya Byzantinos <58523155+Cappu-kyani@users.noreply.github.com>
2022-05-06 21:30:17 +0500

    Transitional update 3

--- interface/scripted/experiment/elementconfiggui.lua
+++ interface/scripted/experiment/elementconfiggui.lua
@@ -1,174 +1,61 @@
-require("/scripts/shared/util.lua")
+
 
 function init()
-	-- init addons
-	elements = {"physical", "ice", "fire", "poison", "electric"}
-	sb.logInfo(elements[1])
 end
 
 
 function update(dt)
-	local weapon = world.containerItemAt(pane.containerEntityId(), 0)
-
+	weapon = world.containerItemAt(pane.containerEntityId(), 0)
+	--sb.logInfo("%s", weapon)
 	checkButtonStatus(weapon)
 end
 
 
 function checkButtonStatus(weapon)
+--	sb.logInfo("%s", root.itemHasTag(weapon.name, "weapon"))
 	craftButtonEnabled = false
 	local errorMessage = ""
-
-		-- continue if weapon has item
+		-- continue if weapon slot is not empty
 		if weapon then
-			if item.hasItemTag("weapon")
-				-- continue if selected level a valid number
-					for k, v in ipairs(elements) do
-						if getText == v then
-						craftButtonEnabled = true
+			 if player.hasCountOfItem("liquidanophium") > 1 then
+				correctTag = root.itemHasTag(weapon.name, "weapon")
+				if correctTag == true then
+								if root.itemConfig("elementalType") == self.selectedElement then
+								craftButtonEnabled = false
+								errorMessage = "^red;- Pick a different element -^white;"
+							else
+								craftButtonEnabled = true
+							end
+						else
+							errorMessage = "^red;- Item must be a weapon -^white;"
 						end
-					end
-					errorMessage = not craftButtonEnabled and "^red;- No such element! -^white;" or ""
 					else
-						errorMessage = "^red;- Pick a different element. -^white;"
+						errorMessage = "^red;- Missing reaction fuel -^white;"
 					end
 				else
-					errorMessage = "^red;- INPUT item must be a weapon! -^white;"
+					errorMessage = "^red;- No weapon to processs -^white;"
 				end
-			else
-				errorMessage = "^red;- No weapon to modify. -^white;"
-			end
-
-	widget.setText("moneyLabel", price)
+	sb.logInfo("%s", root.itemConfig(weapon, "elementalType"))
+	widget.setText("moneyLabel", 1)
 	widget.setText("lblError", errorMessage)
 	widget.setButtonEnabled("craftButton", craftButtonEnabled)
 end
 
+function crafting(weapon)
+		self.elementalType = self.selectElement
+		player.consumeItem({ name = "liquidanophium", count = 1, parameters = {} })
+		world.containerConsumeAt(pane.containerEntityId(), 0)
+		world.containerItemApply(pane.containerEntityId(), {name = weapon.name, parameters={self.elementalType}}, 0)		
+	--	widget.playSound("/sfx/experiment/craftsuccessful_containermanipulatorstation.ogg")
 
-function crafting(weapon,)
-	local getText = widget.getText("spinnerTextbox")
-
-	-- spawn new container and demand resource from player
-	if weapon and not then
-		-- create outputParameters
-		local outputParameters = {
-			elementalType = getText,
-				middle = weapon.name ..elementalType ..png,
-}
-		player.consumeItem({ name = "liquidanophium", count = price, parameters = {} })
-		world.containerConsumeAt(pane.containerEntityId(), 0, weapon.count)
-		world.containerItemApply(pane.containerEntityId(), { name = weapon.name, count = weapon.count, parameters = outputParameters }, 1)
-		widget.playSound("/sfx/experiment/craftsuccessful_containermanipulatorstation.ogg")
-	else
-		widget.playSound("/sfx/experiment/clickon_error.ogg")
-	end
-
-	enableControls()
 end
 
-
-function spinnerInteraction(operator)
-	local getText = widget.getText("spinnerTextbox")
-	for k, v in ipairs(elements) do
-
-		-- increase or decrease slots for comparing
-		if getText == v then 
-			getText = ( operator == "add" ) and getText + 1 or getText - 1
-		end
-		-- is getText between two valid values?
-		if getText < v and getText > elements[1] then
-			return (operator == "add") and v or elements[1]
-
-		-- is getText smaller than max valid value?
-		elseif getText < elements[1] then
-			return (operator == "add") and elements[1] or elements[#elements]
-
-		-- is getText bigger than min valid value?
-		elseif getText > elements[#elements] then
-			 return (operator == "add") and elements[1] or elements[#elements]
-		end
-
-		elements[1] = v
-	end
+function selectElement(widgetName)
+	self.selectedElement = widgetName
 end
 
 
 function craftButton()
-	initialweapon = world.containerItemAt(pane.containerEntityId(), 0)
-	timerStop = false
-	startTime = world.time()
-	widget.setText("spinnerBlockText", "^#3c3c3c;" .. widget.getText("spinnerTextbox"))
-
-	disableControls()
-	widget.playSound("/sfx/experiment/craft_containermanipulatorstation.ogg")
-end
-
-function stopButton()
-	initialweapon = nil
-	timerStop = true
-
-	enableControls()
-	widget.playSound("/sfx/experiment/clickon_error.ogg")
-end
-
-function spinnerTextboxAbort()
-	widget.setText("spinnerTextbox", elements[1])
-	widget.blur("spinnerTextbox")
-end
-
-function spinnerTextboxEnter()
-	widget.blur("spinnerTextbox")
-end
-
-function spinnerPickLeft()
-	newText = spinnerInteraction("sub")
-	widget.setText("spinnerTextbox", newText)
-end
-
-function spinnerPickRight()
-	newText = spinnerInteraction("add")
-	widget.setText("spinnerTextbox", newText)
-end
-
-function enableControls()
-	widget.setVisible("itemGrid", true)
-	widget.setVisible("outputItemGrid", true)
-	widget.setVisible("workingInput", false)
-	widget.setVisible("workingOutput", false)
-
-	widget.setVisible("craftButton", true)
-	widget.setVisible("stopButton", false)
-
-	widget.setVisible("spinnerTextbox", true)
-	widget.setVisible("spinnerBlockText", false)
-	widget.setImage("spinnerTextboxImage", "/interface/scripted/experiment/spinnerTextboxImage.png")
-	widget.setButtonEnabled("spinnerPickLeft", true)
-	widget.setButtonEnabled("spinnerPickRight", true)
-end
-
-function disableControls()
-	widget.setVisible("itemGrid", false)
-	widget.setVisible("outputItemGrid", false)
-	widget.setVisible("workingInput", true)
-	widget.setVisible("workingOutput", true)
-
-	widget.setVisible("craftButton", false)
-	widget.setVisible("stopButton", true)
-
-	widget.setVisible("spinnerTextbox", false)
-	widget.setVisible("spinnerBlockText", true)
-	widget.setImage("spinnerTextboxImage", "/interface/scripted/experiment/spinnerTextboxBlockImage.png")
-	widget.setButtonEnabled("spinnerPickLeft", false)
-	widget.setButtonEnabled("spinnerPickRight", false)
-end
-
-function getSupportedWeapons(weaponName)
-	if not supportedWeaponCache[weaponName] then
-		for k, v in pairs(SupportedWeapons) do
-			if weaponName == k then
-				supportedWeaponCache[weaponName] = k
-				break
-			end
-		end
-	end
-	return supportedWeaponCache[weaponName]
+	sb.logInfo("%s", selectedElement)
+	crafting(weapon)
 end
